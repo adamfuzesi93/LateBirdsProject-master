08c89d5657b365cccfaf460b93c05c0f1ee27198
adam <adamfuzesi93@mailbox.unideb.hu>
2022-04-13 22:43:49 +0200

    probacommit

---
+++ src/main/java/hu/unideb/inf/LateBirds/FXMLSecondPageController.java
@@ -0,0 +1,47 @@
+package hu.unideb.inf.LateBirds;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
+public class FXMLSecondPageController {
+   /* @FXML
+    private ComboBox<?> AdventurePark;
+
+    @FXML
+    private ComboBox<?> ViaFerrata;
+
+    @FXML
+    private ComboBox<?> hiking;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Button logoutButton;
+
+    @FXML
+    private ComboBox<?> motorbike;
+
+    @FXML
+    private ComboBox<?> snowboard;
+
+    @FXML
+    void logoutButtonPressed(ActionEvent event) throws IOException {
+        //Stage stage = (Stage) logoutButton.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("/fxml/FXMLLateBirds.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("LateBirds");
+        stage.setScene(scene);
+        stage.setFullScreen(true);
+        stage.show();
+    }*/
+
+}
