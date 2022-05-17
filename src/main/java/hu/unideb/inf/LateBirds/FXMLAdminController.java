f05dbc1df6975bdc8ea2f481831d578bc8b34e6b
adam <adamfuzesi93@mailbox.unideb.hu>
2022-05-17 12:37:25 +0200

    lastStable

---
+++ src/main/java/hu/unideb/inf/LateBirds/FXMLAdminController.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.LateBirds;
+
+import javafx.event.ActionEvent;
+
+public class FXMLAdminController {
+    public void logoutButtonPushed(ActionEvent actionEvent) {
+    }
+
+    public void deleteButtonPushed(ActionEvent actionEvent) {
+    }
+}
